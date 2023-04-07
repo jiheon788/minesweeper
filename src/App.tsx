1e633fc745f351746553ac94d1f8828f7d0865e2
jiheon788 <jiheon788@ajou.ac.kr>
2023-04-07 16:01:22 +0900

    chore: 리덕스 스토어 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '@/store';
 import DynamicRoutes from './DynamicRoutes';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <DynamicRoutes />
+      <Provider store={store}>
+        <DynamicRoutes />
+      </Provider>
     </BrowserRouter>
   );
 };
