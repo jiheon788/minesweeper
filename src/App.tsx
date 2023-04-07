110f2083e8e40c2142ef67e7867d94aaeeaaee49
jiheon788 <jiheon788@ajou.ac.kr>
2023-04-07 16:23:18 +0900

    style: 스타일 설정 초기화 + 노말라이즈

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
 import DynamicRoutes from './DynamicRoutes';
+import '@/styles/App.scss';
 
 const App = () => {
   return (
